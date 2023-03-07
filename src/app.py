03335fa42a1c18141fd2e7ffe0fb9215dddedfa9
santoshgopane <santoshgopane68@gmail.com>
2023-03-07 19:07:55 +0530

    Dashboard finalized with centered logo.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+from config import CONNECTION_STRING
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def Index():
-    return render_template("index.html")
+
+    Client = MongoClient(CONNECTION_STRING)
+
+    print("DB:", Client.test)
+
+    return render_template("index.html", DB=Client.test)
 
 
 if __name__ == "__main__":
---
+++ src/config.py
@@ -0,0 +1,9 @@
+Username = "santoshgopane"
+Pass = "CUKtzrU9uhhgUeee"
+CONNECTION_STRING = (
+    "mongodb+srv://"
+    + Username
+    + ":"
+    + Pass
+    + "@rebootcv.iztty1c.mongodb.net/?retryWrites=true&w=majority"
+)
