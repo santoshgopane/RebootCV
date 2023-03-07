51567a3a991eac7dbcfd9d521202bb1af16f6662
santoshgopane <santoshgopane68@gmail.com>
2023-03-07 22:05:58 +0530

    Builder prep

---
+++ src/Builder.py
@@ -0,0 +1,11 @@
+import json
+
+"""
+Here are list of things that needs to go in:
+
+1. PDF reader
+2. Readed pdf must go through OCR/ YOLO to extract text
+3. Template PDFs to choose from (create 1-2 template pdf of resumes of same size)
+4. Paraphrase or make it little bit more appealing text.
+5. Reportlab to insert into the pdf
+"""
--- src/app.py
+++ src/app.py
@@ -15,5 +15,10 @@ def Index():
     return render_template("index.html", DB=Client.test)
 
 
+@app.route("/customize")
+def Customize():
+    return "<h1>This page is going to be customizing the pdf!</h1>"
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
